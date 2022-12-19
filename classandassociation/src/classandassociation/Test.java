5802564e4297ce4756cbbc01cc2bc722a48ea291
eric FOE <eric.foe@decathlon.com>
2022-12-19 21:14:50 +0100

    add file on the test

--- classandassociation/src/classandassociation/Test.java
+++ classandassociation/src/classandassociation/Test.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Test {
 	public static void main(String args[]){
-		String a;
-		Employee e=new Employee ("arnold","onana",2002);
-		a=e.getLastName();
-		System.out.println(a);
+		String employeeName;
+		Employee employee=new Employee ("arnold","onana",2002);
+		employeeName=employee.getLastName();
+		System.out.println(employeeName);
 		ObjetNumerique m=new ObjetNumerique("nike","b");
-		e.addObjetNumerique(m);
-		e.display();
+		employee.addObjetNumerique(m);
+		employee.display();
 	}
-	
+
 
 }
